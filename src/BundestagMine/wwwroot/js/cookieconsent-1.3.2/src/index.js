20b9ed00a8eb3960446764a8fa901ddf4966ba3c
immunizedcarbon <98087886+immunizedcarbon@users.noreply.github.com>
2025-09-17 11:04:54 +0200

    Delete src directory

--- src/BundestagMine/wwwroot/js/cookieconsent-1.3.2/src/index.js
+++
@@ -1,6 +0,0 @@
-import CookieConsent from './lib/CookieConsent';
-
-const cookieConsent = new CookieConsent();
-
-window.CookieConsent = window.CookieConsent || {};
-window.CookieConsent.init = cookieConsent.init;

f006dfc2beacaca5a1f8b9dd74a0154ffbfb2985
immunizedcarbon <98087886+immunizedcarbon@users.noreply.github.com>
2025-09-17 13:43:40 +0200

    Expose ProtocolOverview in database package

--- src/mine/database/__init__.py
+++ src/mine/database/__init__.py
@@ -2,12 +2,13 @@
 from __future__ import annotations
 
 from .models import Base, Protocol, SpeechModel
-from .storage import Storage, create_storage
+from .storage import ProtocolOverview, Storage, create_storage
 
 __all__ = [
     "Base",
     "Protocol",
     "SpeechModel",
+    "ProtocolOverview",
     "Storage",
     "create_storage",
 ]
